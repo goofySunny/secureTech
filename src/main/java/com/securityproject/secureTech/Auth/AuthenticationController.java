292423f483147d79b06718586b52540afbc428f1
Ali najafian <alinajafian580@gmail.com>
2024-05-02 16:46:18 +0330

    Removed a pesky useless import

--- src/main/java/com/securityproject/secureTech/Auth/AuthenticationController.java
+++ src/main/java/com/securityproject/secureTech/Auth/AuthenticationController.java
@@ -1,7 +1,5 @@
 package com.securityproject.secureTech.Auth;
 
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
